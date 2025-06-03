8793ceb0338ea18dbd0801db0f847ea5a6e40b20
RoberGF <rgfrasta@gmail.com>
2025-06-03 17:07:21 +0200

    Fix css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/*Theme toggle*/}
-        {/*Bg effects*/}
+        <ThemeToggle/>        {/*Bg effects*/}
         {/*Nav bar*/}
         {/*Main Content*/}
         {/*Footer*/}
