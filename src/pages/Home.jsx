315511489931c6cbbb41038adc9fc8ed869d8d25
RoberGF <rgfrasta@gmail.com>
2025-06-03 12:19:31 +0200

    setting up

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return <div>
+        {/*Theme toggle*/}
+        {/*Bg effects*/}
+        {/*Nav bar*/}
+        {/*Main Content*/}
+        {/*Footer*/}
+    </div>
+}
