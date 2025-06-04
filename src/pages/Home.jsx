355607af001e89e411b4fc9c5f8ebdc945d89e4d
RoberGF <rgfrasta@gmail.com>
2025-06-04 12:13:29 +0200

    random stars background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import { ThemeToggle } from "../components/ThemeToggle"
+import { ThemeToggle } from "@/components/ThemeToggle"
+import { StarBackground } from "@/components/StarBackground"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/*Theme toggle*/}
-        <ThemeToggle/>        {/*Bg effects*/}
+        <ThemeToggle/>        
+        {/*Bg effects*/}
+        <StarBackground />
         {/*Nav bar*/}
         {/*Main Content*/}
         {/*Footer*/}
