36173751951839aea36edc5434f017d4d98158b6
RoberGF <rgfrasta@gmail.com>
2025-06-05 10:42:22 +0200

    HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowBigDownDash } from "lucide-react";
 import { useState, useEffect } from "react";
 
 export const HeroSection = () => {
@@ -14,11 +15,10 @@ export const HeroSection = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prev => (prev + 1) % phrases.length);
+      setIndex((prev) => (prev + 1) % phrases.length);
     }, 5000);
     return () => clearInterval(interval);
   }, []);
-
 
   return (
     <section
@@ -36,9 +36,10 @@ export const HeroSection = () => {
               Gómez
             </span>
           </h1>
-          <p 
-          key={index}
-          className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-out">
+          <p
+            key={index}
+            className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-out"
+          >
             {phrases[index]}
           </p>
           <div className="pt-5 opacity-0 animate-fade-in-delay-7">
@@ -46,6 +47,13 @@ export const HeroSection = () => {
               Check my Work!
             </a>
           </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 opacity-0 animate-fade-in-delay-4s ">
+        <div className="animate-bounce flex flex-col items-center">
+          <span className="text-sm text-muted-foreground ">Scroll</span>
+          <ArrowBigDownDash className="h-5 w-4 text-primary" />
         </div>
       </div>
     </section>
