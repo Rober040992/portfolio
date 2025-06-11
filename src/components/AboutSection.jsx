bb832808e8ccdb081075b2cb80448e947b263951
RoberGF <rgfrasta@gmail.com>
2025-06-11 18:51:18 +0200

    about section translate

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,33 +1,21 @@
 import { Briefcase, Code, User } from "lucide-react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export const AboutSection = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   return (
     <section id="about" className="py-24 px-4 relative">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-14 text-center">
-          {t("about.about")} <span className="text-primary">{t("about.me")}</span>
+          {t("about.about")}{" "}
+          <span className="text-primary">{t("about.me")}</span>
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
-            <h3 className="font-semibold text-2xl">
-              {" "}
-              Passionate developer blablabla
-            </h3>
-            <p className="text-muted-foreground">
-              habla de experiencia bla bla Lorem, ipsum dolor sit amet
-              consectetur adipisicing elit. Optio velit officia perferendis
-              inventore, adipisci molestias saepe maiores error possimus totam
-              amet facilis quis autem dolore magni ipsa soluta fugiat quidem.
-            </p>
-            <p className="text-muted-foreground">
-              apasionado en bla bla Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Nesciunt, earum voluptate! Rem fugiat quam saepe
-              sequi, ea accusamus perspiciatis et illo repellendus animi ad
-              corrupti cum! Quibusdam cumque impedit voluptas!
-            </p>
+            <h3 className="font-semibold text-2xl"> {t("about.title")}</h3>
+            <p className="text-muted-foreground">{t("about.1paragraph")}</p>
+            <p className="text-muted-foreground">{t("about.2paragraph")}</p>
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">
                 {t("about.touch")}
@@ -50,11 +38,9 @@ export const AboutSection = () => {
                 </div>
                 <div className="text-left">
                   <h4 className="font-semibold text-primary">
-                    Web Development
+                    {t("services.webDevTitle")}
                   </h4>
-                  <p>
-                    Creating modern websites and web apps using modern tools
-                  </p>
+                  <p>{t("services.webDevDesc")} </p>
                 </div>
               </div>
             </div>
@@ -64,11 +50,10 @@ export const AboutSection = () => {
                   <User className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-primary">UI/UX</h4>
-                  <p>
-                    Designing intuitive and inclusive user experiences focused
-                    on usability and clarity
-                  </p>
+                  <h4 className="font-semibold text-primary">
+                    {t("services.uiuxTitle")}
+                  </h4>
+                  <p>{t("services.uiuxDesc")}</p>
                 </div>
               </div>
             </div>
@@ -79,12 +64,9 @@ export const AboutSection = () => {
                 </div>
                 <div className="text-left">
                   <h4 className="font-semibold text-primary">
-                    Team Collaboration
+                    {t("services.teamTitle")}{" "}
                   </h4>
-                  <p>
-                    Working in agile teams, applying Git and GitHub for
-                    efficient collaboration
-                  </p>
+                  <p>{t("services.teamDesc")}</p>
                 </div>
               </div>
             </div>
