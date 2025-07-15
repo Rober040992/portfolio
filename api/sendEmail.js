4af2ce4dcd1630031d96ba13cfdd6829cc56c3c1
RoberGF <rgfrasta@gmail.com>
2025-07-15 12:34:55 +0200

    minor fix

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -8,13 +8,13 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
+  const { name, email, message, honeypotValue, recaptchaToken  } = req.body;
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!emailRegex.test(email)) {
     return res.status(400).json({ error: "Invalid email format" });
   }
 
-  const { name, email, message, honeypotValue, recaptchaToken  } = req.body;
 
   // Honeypot check
   if (honeypotValue) {
